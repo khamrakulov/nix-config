cfbe38d61da9de7b0ddf312fc30ab1688825779f
Kamron Khamrakulov <khamrakulovkamron@gmail.com>
2024-09-03 18:46:07 +0500

    switched to hyprland

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -74,12 +74,17 @@
   i18n.defaultLocale = "en_US.utf8";
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver = {
-    # Enable the X11 windowing system.
+  # services.xserver = {
+  #   # Enable the X11 windowing system.
+  #   enable = true;
+  #   displayManager.sddm.enable = true;
+  #   desktopManager.plasma5.enable = true;
+  # };
+
+  xdg.portal = {
     enable = true;
-    displayManager.sddm.enable = true;
-    desktopManager.plasma5.enable = true;
-  };
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  }
 
   # Configure keymap in X11
   services.xserver = {
@@ -94,21 +99,14 @@
 
   # Enable sound with pipewire.
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  security.sudo.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+    jack.enable = true;
+  }
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -137,9 +135,17 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
     kmod
+    (waybar.overrideAttrs (oldAttrs: {
+      mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+    }))
+    dunst
+    libnotify
+    swww
+    kitty
+    rofi-wayland
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -153,6 +159,30 @@
     enable = true;
     defaultEditor = true;
   };
+
+  # enable hyprland
+  programs.hyprland = {
+    enable = true;
+    nvidiaPatches = true;
+    xwayland = {
+      enable = true;
+    }
+  };
+
+  environment.sessionVariables = {
+    # Cursor invisibility
+    WLR_NO_HARDWARE_CURSORS = "1";
+    # Hint electron apps to use wayland
+    NIXOS_OZONE_WL = "1";
+  };
+
+  hardware = {
+    # Opengl
+    opengl.enable = true;
+
+    # Most wayland compositors need this
+    nvidia.modesetting.enable = true;
+  }
 
   services.openssh = {
     enable = true;
