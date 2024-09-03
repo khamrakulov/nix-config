64395c61f4bf36c4f60acb7a40fdf2153d66c5ed
Kamron Khamrakulov <khamrakulovkamron@gmail.com>
2024-09-03 18:23:44 +0500

    changed from modprobe to kmod

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -139,7 +139,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-    modprobe
+    kmod
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
