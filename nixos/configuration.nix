913eb685968576a904b3708f380250916e9d5d27
Kamron Khamrakulov <khamrakulovkamron@gmail.com>
2024-09-03 18:21:23 +0500

    added modproble on env

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -131,6 +131,7 @@
       vscode
       nodejs
       jetbrains-toolbox
+      libre-
     ];
   };
 
@@ -139,6 +140,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    modprobe
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
